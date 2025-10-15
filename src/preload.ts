b5dffa0cb0d27a47572e1800c457da60b8771a74
abrahamVado <abraham.vado@gmail.com>
2025-10-14 23:09:10 -0600

    chore(starting): adding initial commit

--- src/preload.ts
+++
@@ -1,4 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-const NS = 'lingua' as const;
-const api = { ping: () => ipcRenderer.invoke(`${NS}:ping`) };
-contextBridge.exposeInMainWorld(NS, api);
